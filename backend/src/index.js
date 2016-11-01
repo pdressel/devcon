16362c1a2caa4cab776fa74c56c83ee890561406
Philipp Dressel <philipp.dressel@gmail.com>
2016-11-01 10:47:31 +0100

    Added basic service access

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
 
 app.get('/', function (req, res) {
-  res.send('Hello from the backend!');
+	MongoClient.connect(process.env.MONGODB_URL, function(err, db) {
+	  if(err) {
+			res.send('Failed to reach mongodb 😐');
+		} else {
+			res.send("Connected to mongodb!");
+		  db.close();
+		}
+	});
 });
 
-app.listen(3000, function () {
-  console.log('Backend listening on port 3000!');
+app.listen(process.env.PORT, function () {
+  console.log(`Backend listening on port ${process.env.PORT}!`);
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+var request = require('superagent');
 
 app.get('/', function (req, res) {
-  res.send('Hello from the frontend!');
+	request.get(process.env.BACKEND_URL).end(function (err, result) {
+    if (err) {
+			res.send('Failed to reach the backend 😐');
+		} else {
+			res.send(`Response from the backend: "${result.text}"`);
+		}
+  });
 });
 
-app.listen(3000, function () {
-  console.log('Frontend listening on port 3000!');
+app.listen(process.env.PORT, function () {
+  console.log(`Frontend listening on port ${process.env.PORT}!`);
 });
